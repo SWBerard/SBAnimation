e63fa4b78c56bcbec0d95ccb795946727925e186
Steven Berard <Steven.Berard@eventfarm.com>
2017-02-20 20:12:54 -0800

    Initial project creation

---
+++ SBAnimation/SBAnimation.h
@@ -0,0 +1,19 @@
+//
+//  SBAnimation.h
+//  SBAnimation
+//
+//  Created by Steven Berard on 2/20/17.
+//  Copyright © 2017 Steven Berard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SBAnimation.
+FOUNDATION_EXPORT double SBAnimationVersionNumber;
+
+//! Project version string for SBAnimation.
+FOUNDATION_EXPORT const unsigned char SBAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBAnimation/PublicHeader.h>
+
+
